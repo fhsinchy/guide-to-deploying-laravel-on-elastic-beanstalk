b3c47615d957bbbd64a3c183b216ac7314f39491
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-07-08 12:20:50 +0600

    Added email verification

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ Route::resource('answers', App\Http\Controllers\AnswerController::class)->only([
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
