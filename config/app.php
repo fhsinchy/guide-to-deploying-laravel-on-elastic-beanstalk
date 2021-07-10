8790194adad474bb7065612b360e517a21cb5d84
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-07-10 13:16:48 +0600

    Updated application name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Question Board'),
+    'name' => env('APP_NAME', 'Laravel On Beanstalk'),
 
     /*
     |--------------------------------------------------------------------------
