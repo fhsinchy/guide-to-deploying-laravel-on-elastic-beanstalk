36424adee019bb3603ebd6202754401dc030f1df
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-07-14 14:51:32 +0600

    Added admin user seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::create([
+            'name' => 'Farhan Hasin Chowdhury',
+            'email' => 'farhan@laravel-on-beanstalk.site',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
