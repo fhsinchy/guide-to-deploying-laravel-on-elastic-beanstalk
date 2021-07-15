7ea734a10686615efddb454f21bf66dedef28aa7
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-07-15 21:54:08 +0600

    Brought back certbot

--- .platform/hooks/postdeploy/auto_renew_ssl_certificate.sh
+++ .platform/hooks/postdeploy/auto_renew_ssl_certificate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# echo "0 0 1 * * certbot renew --no-self-upgrade" \
-#     | sudo tee /etc/cron.d/renew_ssl_cert_cron
+echo "0 0 1 * * certbot renew --no-self-upgrade" \
+    | sudo tee /etc/cron.d/renew_ssl_cert_cron
