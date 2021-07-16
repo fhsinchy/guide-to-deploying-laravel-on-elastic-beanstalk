f3c9e4249857f0742fda010e00e5b2f762fb933c
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-07-16 17:36:11 +0600

    Enabled ssl cert

--- .platform/hooks/postdeploy/get_ssl_certificate.sh
+++ .platform/hooks/postdeploy/get_ssl_certificate.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# sudo certbot \
-#     -n \
-#     --nginx \
-#     --agree-tos \
-#     -d $(/opt/elasticbeanstalk/bin/get-config environment -k CERTBOT_DOMAINS) \
-#     --email $(/opt/elasticbeanstalk/bin/get-config environment -k CERTBOT_EMAIL)
+sudo certbot \
+    -n \
+    --nginx \
+    --agree-tos \
+    -d $(/opt/elasticbeanstalk/bin/get-config environment -k CERTBOT_DOMAINS) \
+    --email $(/opt/elasticbeanstalk/bin/get-config environment -k CERTBOT_EMAIL)
